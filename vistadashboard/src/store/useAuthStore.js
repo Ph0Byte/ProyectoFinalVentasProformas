d8cfe6f6618eb6b0db5167e142c7f5fe52290761
Ph0Byte <kartonjuan4@gmail.com>
2024-11-08 19:53:19 -0500

    Update useAuthStore.js

--- vistadashboard/src/store/useAuthStore.js
+++ vistadashboard/src/store/useAuthStore.js
@@ -1,6 +1,5 @@
 import { create } from 'zustand';
 
-// Lee el estado de autenticación y usuario desde localStorage al iniciar
 const initialState = {
   isAuthenticated: localStorage.getItem('isAuthenticated') === 'true',
   user: JSON.parse(localStorage.getItem('user')) || null,
@@ -9,16 +8,13 @@ const initialState = {
 export const useAuthStore = create((set) => ({
   ...initialState,
   login: (userData) => {
-    // Guarda el estado de autenticación y usuario en el store
     set({ isAuthenticated: true, user: userData });
     // Almacena en localStorage
     localStorage.setItem('isAuthenticated', 'true');
     localStorage.setItem('user', JSON.stringify(userData));
   },
   logout: () => {
-    // Restablece el estado de autenticación y usuario en el store
     set({ isAuthenticated: false, user: null });
-    // Remueve los datos de localStorage
     localStorage.removeItem('isAuthenticated');
     localStorage.removeItem('user');
   },
